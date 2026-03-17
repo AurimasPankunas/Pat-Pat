1618d497bd9ee1d2ab4b21f7f1b1807ca191a528
Aurimas Pankunas <aurpan@ktu.lt>
2026-03-17 19:01:56 +0200

    Added a simple long cube as a conveyor and added a test button for now to show the conveyor working

---
+++ PatPatGame/Assets/Scripts/ConveyorBelt.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour
+{
+    public float speed;
+    public Vector3 direction;
+    public List<GameObject> onBelt;
+    private GameObject Item;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        onBelt = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        for(int i = 0; i < onBelt.Count; i++)
+        {
+            onBelt[i].GetComponent<Rigidbody>().linearVelocity = speed * direction * Time.deltaTime;
+        }
+    }
+
+    // When something collides with the belt
+    void OnCollisionEnter(Collision collision)
+    {
+        onBelt.Add(collision.gameObject);
+    }
+
+    // When something leaves the belt unfreeze the rotations
+    void OnCollisionExit(Collision collision)
+    {
+        onBelt.Remove(collision.gameObject);
+        Rigidbody item = collision.gameObject.GetComponent<Rigidbody>();
+        item.constraints = RigidbodyConstraints.None;
+
+    }
+
+    // Spawn an item on the belt and freeze its rotations
+    public void SpawnItemOnBelt(GameObject gameObject)
+    {
+        Item = gameObject;
+        Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+        gameObject.transform.position = transform.position + Vector3.up;
+    }
+}
