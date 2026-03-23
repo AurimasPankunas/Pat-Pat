238ca61c44f020f756b67516fec95d4bde532283
CheesyRobot <ievalt2020@gmail.com>
2026-03-23 02:45:28 +0200

    Scripts, Prefabs and MainScene
    Shop generates items from ShopItemDatabase ScriptableObject which is available in Prefabs folder
    ShopManager is where all the main functions for handling the shop are. In the scene it's located in the Systems prefab
    Added SubtractMoney to PlayerBalance script, used it in ShopManager
    Features a small animation when items are bought and player is unable to buy things that are too expensive
    Shop items are not sorted in any way at this time, they depend on the order in the ShopItemDatabase

---
+++ PatPatGame/Assets/Scripts/UI/UIShopElement.cs
@@ -0,0 +1,119 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class UIShopElement
+{
+    public ShopItem shopItem;
+    private ShopManager shopManager;
+
+    public TemplateContainer _shopElement;
+    private Label _itemName;
+    private Image _itemImage;
+    private Button _buyButton;
+    private Label _price;
+
+    private Label _description;
+    private Label _rarity;
+
+    // Pictures for no image textures in items
+    private Texture2D waterImg = Resources.Load<Texture2D>("Textures/Water");
+    private Texture2D foodImg = Resources.Load<Texture2D>("Textures/Food");
+    private Texture2D gloveImg = Resources.Load<Texture2D>("Textures/Glove");
+
+    public UIShopElement(VisualTreeAsset templateContainer, ShopItem shopItem, ShopManager shopManager)
+    {
+        this.shopManager = shopManager;
+        this.shopItem = shopItem;
+        // Instantiate visual element
+        this._shopElement = templateContainer.Instantiate();
+        _shopElement.name = templateContainer.name;
+        // Get main UI elements from UI document
+        _itemName = _shopElement.Q<Label>("ElementLabel");
+        _itemImage = _shopElement.Q<Image>("ElementImage");
+        _buyButton = _shopElement.Q<Button>("ElementBuyButton");
+        _price = _shopElement.Q<Label>("ElementPriceLabel");
+
+        // Set price and register button to a function
+        _price.text = shopItem.price.ToString();
+        _buyButton.RegisterCallback<ClickEvent>(OnClick);
+
+        // Set the rest of the UI elements depending on item type
+        switch (shopItem)
+        {
+            case FoodItem:
+                FoodItem fitem = (FoodItem) shopItem;
+                _itemName.text = fitem.name;
+                if (fitem.image){
+                    _itemImage.image = fitem.image;
+                }
+                else { _itemImage.image = foodImg; }
+                break;
+            case WaterItem:
+                WaterItem witem = (WaterItem)shopItem;
+                _itemName.text = witem.name;
+                if (witem.image){
+                    _itemImage.image = witem.image;
+                }
+                else { _itemImage.image = waterImg; }
+                break;
+            case GloveItem:
+                GloveItem gitem = (GloveItem)shopItem;
+                _rarity = _shopElement.Q<Label>("GloveRarity");
+                _itemName.text = gitem.type;
+                if (gitem.image){
+                    _itemImage.image = gitem.image;
+                }
+                else { _itemImage.image = gloveImg;}
+                // Image color tint of glove image vvv
+                // Remove if removing imageTint from GloveItem class
+                if(gitem.imageTint != null){
+                    Color tint = new Color(gitem.imageTint.r, gitem.imageTint.g, gitem.imageTint.b);
+                    _itemImage.tintColor = tint;
+                }
+
+                if (_rarity != null){
+                    _rarity.text = gitem.rarity.ToString();
+                }
+                break;
+            default:
+                break;
+        }
+
+        // In case ShopItemElementDescription template would be used eventually
+        //_description = _shopElement.Q<Label>("ElementDescription");
+        SetEnabledIfBalanceIsEnough();
+    }
+
+    public void SetEnabledIfBalanceIsEnough()
+    {
+        VisualElement elementToDisable;
+        if (_shopElement.name == "ShopItemElementLong") {
+            elementToDisable = _buyButton;
+        }
+        else elementToDisable = _shopElement;
+        if (shopManager.playerBalance.money < shopItem.price)
+        {
+            if (elementToDisable.enabledSelf)
+            {
+                elementToDisable.SetEnabled(false);
+            }
+        }
+        else
+        {
+            if (!elementToDisable.enabledSelf)
+            {
+                elementToDisable.SetEnabled(true);
+            }
+        }
+    }
+
+    void OnClick(ClickEvent evt)
+    {
+        shopManager.ShopItemClicked(shopItem);
+    }
+    
+    public void OnDisable()
+    {
+        _buyButton.UnregisterCallback<ClickEvent>(OnClick);
+    }
+}
