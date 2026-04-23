52909dc34f005d40ffd9f17d7da7ac62df86346a
Aurimas Pankunas <aurpan@ktu.lt>
2026-04-23 12:50:17 +0300

    Removed istrigger colliders from food/drinks, added istrigger to feeding collider of animals and added is trigger to tray to hold the items

--- PatPatGame/Assets/Scripts/Entities/Animal_Stat_Increase.cs
+++ PatPatGame/Assets/Scripts/Entities/Animal_Stat_Increase.cs
@@ -44,7 +44,7 @@ public class Animal_Stat_Increase : MonoBehaviour
                     animal.UpdateFood(foodData.foodAmount);
                     animal.GetComponent<Animator>().SetTrigger("Eat");
                     Message = null;
-                    Destroy(other.transform.root.gameObject);
+                    Destroy(other.gameObject.transform.parent.gameObject);
                 } else
                 {
                     Message = "Wrong rarity food!";
@@ -64,7 +64,7 @@ public class Animal_Stat_Increase : MonoBehaviour
                     animal.UpdateWater(waterData.waterAmount);
                     animal.GetComponent<Animator>().SetTrigger("Eat");
                     Message = null;
-                    Destroy(other.transform.root.gameObject);
+                    Destroy(other.gameObject.transform.parent.gameObject);
                 } else
                 {
                     Message = "Wrong rarity drink!";
