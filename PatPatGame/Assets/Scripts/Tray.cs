b6548dd4c749f6fd32e2ee13dfde98eed74655b2
CheesyRobot <ievalt2020@gmail.com>
2026-04-25 00:22:02 +0300

    Different placing object tactic, ContainedObject Layer

--- PatPatGame/Assets/Scripts/Tray.cs
+++ PatPatGame/Assets/Scripts/Tray.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using System.Collections;
+using Unity.XR.CoreUtils;
 
 public class Tray : MonoBehaviour
 {
@@ -9,28 +11,58 @@ public class Tray : MonoBehaviour
         Rigidbody item = other.gameObject.GetComponentInParent<Rigidbody>();
         if (item != null)
         {
-            if(other.CompareTag("Food") || other.CompareTag("Drink")) {
+            if((other.CompareTag("Food") || other.CompareTag("Drink")) && !item.isKinematic) {
                 // Freeze the item's physics
-                item.constraints = RigidbodyConstraints.FreezeAll;
-            
-                // Parent the item (parent and all children) to the tray so it moves with it
-                item.gameObject.transform.SetParent(transform);
-
-                var grabInteractable = item.gameObject.GetComponent<XRGrabInteractable>();
-                if (grabInteractable != null)
-                {
-                    grabInteractable.selectEntered.AddListener((SelectEnterEventArgs args) => RemoveFromTray(item));
-                }
+                //Debug.Log("TriggerEnter");
+                PlaceObject(item.gameObject);
             }
         }
     }
 
-    void RemoveFromTray(Rigidbody item)
+    /// <summary>
+    /// Freeze an object in the tray as its parent and setting its rigidbody as Kinematic
+    /// </summary>
+    void PlaceObject(GameObject obj)
     {
-        if (item != null)
-        {
-            item.gameObject.transform.SetParent(null);
-            item.constraints = RigidbodyConstraints.None;
-        }
+        //Debug.Log("Placing "+obj.name);
+        Rigidbody rb = obj.GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+
+        obj.transform.SetParent(transform, true);
+        // Set layer to ContainedObject, in order for tray's Rigidbody
+        // to ignore collisions with objects inside it
+        obj.SetLayerRecursively(6);
+        
+        
+        // Setting movement type for interactable and
+        // adding listener so object can get removed from tray
+        XRGrabInteractable grabInteractable = obj.GetComponent<XRGrabInteractable>();
+        grabInteractable.movementType = XRBaseInteractable.MovementType.Kinematic;
+        grabInteractable.selectExited.AddListener(RemoveObject);
+    }
+
+    /// <summary>
+    /// Unfreeze an object from the tray
+    /// </summary>
+    private void RemoveObject(BaseInteractionEventArgs args)
+    {
+        GameObject obj = args.interactableObject.transform.gameObject;
+        RemoveObject(obj);
+    }
+
+    private void RemoveObject(GameObject obj)
+    {
+        //Debug.Log("Removing "+obj.name);
+        obj.transform.SetParent(null);
+        Rigidbody rb = obj.GetComponent<Rigidbody>();
+        rb.isKinematic = false;
+        // Set layer back to Default
+        obj.SetLayerRecursively(0);
+        rb.constraints = RigidbodyConstraints.None;
+
+        XRGrabInteractable grabInteractable = obj.GetComponent<XRGrabInteractable>();
+        grabInteractable.movementType = XRBaseInteractable.MovementType.VelocityTracking;
+        //reset its eventListeners
+        grabInteractable.selectExited.RemoveListener(RemoveObject);
     }
 }
